d256a93e2320d4b7c775a26d15490ae2ff5c69f5
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-10-02 14:31:08 +0530

    Learned data types

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Joe Ambooken";
+    int age = 24;
+    float weight = 55.7;
+    char alphabet = 'A';
+    bool married = false;
+    cout << "My name is " << name << ". I am " << age << " years old." << " I weigh " << weight << " kgs." << " My favorite alphabet is " << alphabet << "." << endl;
+    cout << "Married Status: " << married << endl;
+    return 0;
+}
